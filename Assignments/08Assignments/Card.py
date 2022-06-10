36f24dcd34640db849e9f3c126d71886bb5e639c
Thibo V <r0886870@student.thomasmore.be>
2022-06-10 21:41:08 +0200

    deleted assignment

--- Assignments/08Assignments/Card.py
+++
@@ -1,8 +0,0 @@
-class Card():
-    def __init__(self, cardstring):
-        self.suit = cardstring[-1]
-        if (len(cardstring) == 2):
-            self.value = cardstring[0]
-        else:
-            self.value = cardstring[0] + cardstring[1]
-            
